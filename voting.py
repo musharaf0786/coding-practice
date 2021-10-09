906bf636b10a03863a76b0260ef167aec5502c3f
Your Name <mdmusharafkamal786@gmail.com>
2021-10-09 12:28:40 +0530

    checking for voting

---
+++ voting.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age:-"))
+name=input("enter the name of candidate:")
+if age>18:
+    print("hey" ,name, "you are eligible for voting")
+else:
+    print("hey", name, "you are not eligible for voting because you are kids ")
+
